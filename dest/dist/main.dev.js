9f3003c69446d06f0a3d3803fd32e7e49d131fa4
quoc anh <quocanh.buinguyen@gmail.com>
2023-12-20 23:45:00 +0700

    commit 20/12 11:44pm

--- dest/dist/main.dev.js
+++ dest/dist/main.dev.js
@@ -20,11 +20,40 @@ $(document).ready(function () {
       header.addClass('--scroll');
       $('.overlay').removeClass('active');
     } else header.removeClass('scroll');
-  }); // scroll up
+  }); // run all works tabs function
+
+  allworksTabs(); // scroll up
 
   $(window).scroll(function () {
     var scrollTop = $('#scroll-top');
     if (scrollTop) pageYOffset > window.innerHeight ? scrollTop.addClass('show') : scrollTop.removeClass('show');
   });
-});
+}); // all works Tabs function
+
+function allworksTabs() {
+  // Tabs functionality
+  $('.allworks__tabs').each(function (e) {
+    // Hide all tab panels except for the first
+    $('.allworks__content-panel').not(':first').hide(); // Add active statuses to first tab and show display
+
+    $('.allworks__tabs-item', this).removeClass('active');
+    $('.allworks__tabs-item:first-child', this).addClass('active');
+    $('.allworks__content-panel:first-child').show(); // Tab clicked
+
+    $('.allworks__tabs-item', this).click(function (e) {
+      // Prevent the anchor's default click action
+      e.preventDefault(); // Corresponding tabs panel
+
+      var panel = $('a', this).attr('href'); // Remove active statuses to other tabs
+
+      $(this).siblings().removeClass('active'); // Add active status to this tab
+
+      $(this).addClass('active'); // Hide other tab panels
+
+      $(panel).siblings().hide(); // Showing the clicked tabs' panel
+
+      $(panel).fadeIn(400);
+    });
+  });
+}
 //# sourceMappingURL=main.dev.js.map
--- dest/main.js
+++ dest/main.js
@@ -21,6 +21,9 @@ $(document).ready(function () {
             header.removeClass('scroll')
 
     })
+    // run all works tabs function
+    allworksTabs();
+
     // scroll up
     $(window).scroll(() => {
         var scrollTop = $('#scroll-top')
@@ -28,3 +31,38 @@ $(document).ready(function () {
             pageYOffset > window.innerHeight ? scrollTop.addClass('show') : scrollTop.removeClass('show')
     })
 })
+
+// all works Tabs function
+function allworksTabs() {
+    // Tabs functionality
+    $('.allworks__tabs').each(function (e) {
+        // Hide all tab panels except for the first
+        $('.allworks__content-panel').not(':first').hide();
+
+        // Add active statuses to first tab and show display
+        $('.allworks__tabs-item', this).removeClass('active');
+        $('.allworks__tabs-item:first-child', this).addClass('active');
+        $('.allworks__content-panel:first-child').show();
+
+        // Tab clicked
+        $('.allworks__tabs-item', this).click(function (e) {
+            // Prevent the anchor's default click action
+            e.preventDefault();
+
+            // Corresponding tabs panel
+            var panel = $('a', this).attr('href');
+
+            // Remove active statuses to other tabs
+            $(this).siblings().removeClass('active');
+
+            // Add active status to this tab
+            $(this).addClass('active');
+
+            // Hide other tab panels
+            $(panel).siblings().hide();
+
+            // Showing the clicked tabs' panel
+            $(panel).fadeIn(400);
+        });
+    });
+}
